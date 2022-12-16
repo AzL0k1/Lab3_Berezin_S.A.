aace2af68073a6d5fa646cb0f1708cb7467e0412
AzL0k1 <120719417+AzL0k1@users.noreply.github.com>
2022-12-16 17:31:22 +0700

    Add files via upload

---
+++ bsa/new_project2.cs
@@ -0,0 +1,5 @@
+8
+800
+555
+3535
+Berezin - cemen.berezin@mail.ru
---
+++ cloned_bsa/new_project.cs
@@ -0,0 +1,6 @@
+The first line of code
+The second line of code
+The third line of code
+Finish
+Berezin - cemen.berezin@mail.ru
+Last line
